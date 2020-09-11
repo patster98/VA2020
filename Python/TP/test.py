dfcfbbca0a0538d5fe9d6c044cd176cc2474014c
patster98 <57993483+patster98@users.noreply.github.com>
2020-09-11 19:40:42 -0300

    changes 11/9/2020 added to test.py: HuMoments, moments, and log transform set compare when pressing c contour filter for max and min area set with trackbars find max cnt area from filtered list if statement lifesaver (line 64)

--- Python/TP/test.py
+++ Python/TP/test.py
@@ -5,75 +5,90 @@ def on_trackbar_change(val):
     # do nothing
     pass
 
-window_name = 'Binary_manualThresh'
-window_name2 = 'Binary_autoThresh'
+window_name = 'contourImg'
+window_name2 = 'BinaryImg'
+
 trackbar_name = 'ThreshVal'
 trackbar2_name = 'MaxThresh'
+trackbar3_name = 'MaxContArea'
+trackbar4_name = 'MinContArea'
+
+
 cv2.namedWindow(window_name)
 cv2.namedWindow(window_name2)
 
 slider_max = 200
 slider2_max = 255
-cv2.createTrackbar(trackbar_name, window_name, 50, slider_max, on_trackbar_change)
-cv2.createTrackbar(trackbar2_name, window_name, 50, slider2_max, on_trackbar_change)
-cv2.createTrackbar(trackbar2_name, window_name2, 50, slider2_max, on_trackbar_change)
+
+cv2.createTrackbar(trackbar_name, window_name, 150, slider_max, on_trackbar_change)
+cv2.createTrackbar(trackbar2_name, window_name, 200, slider2_max, on_trackbar_change)
+cv2.createTrackbar(trackbar3_name, window_name, 8000, 10000, on_trackbar_change)
+cv2.createTrackbar(trackbar4_name, window_name, 100, 1000, on_trackbar_change)
 
 
 def Tp():
 
     cap = cv2.VideoCapture(0)
+    compare = None
 
     while True:
+        filtered = []
         _, frame = cap.read()
-        _, frame2 = cap.read()
-
 
         threshVal_trackbar = cv2.getTrackbarPos(trackbar_name, window_name)
         maxThresh_trackbar = cv2.getTrackbarPos(trackbar2_name, window_name)
-        maxThresh_trackbar2 = cv2.getTrackbarPos(trackbar2_name, window_name2)
-
+        maxCnt = cv2.getTrackbarPos(trackbar3_name, window_name)
+        minCnt = cv2.getTrackbarPos(trackbar4_name, window_name)
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 
         _, thresh1 = cv2.threshold(gray, threshVal_trackbar, maxThresh_trackbar, cv2.THRESH_BINARY)
-        _, threshAuto = cv2.threshold(gray2, 127, maxThresh_trackbar2, cv2.THRESH_OTSU)
 
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
+        # kernel2 = cv2.getGaussianKernel(ksize=3)
         opening1 = cv2.morphologyEx(thresh1, cv2.MORPH_OPEN, kernel)
         closing1 = cv2.morphologyEx(opening1, cv2.MORPH_CLOSE, kernel)
-        opening2 = cv2.morphologyEx(threshAuto, cv2.MORPH_OPEN, kernel)
-        closing2 = cv2.morphologyEx(opening2, cv2.MORPH_CLOSE, kernel)
 
-        contDnis1 = cv2.findContours(closing1, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        contDnis2 = cv2.findContours(closing2, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        contDnis1, _ = cv2.findContours(closing1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         # sin este if se rompe el programa cuando el trackbar esta en 0
-        if (threshVal_trackbar and maxThresh_trackbar and maxThresh_trackbar2) == 0:
+        if (threshVal_trackbar and maxThresh_trackbar) == 0:
             cv2.drawContours(frame, contDnis1, 0, (0, 0, 255), 6)  # sobre threshMan
-            cv2.drawContours(frame2, contDnis2, 0, (0, 255, 255), 6)  # sobre threshAuto
 
         else:
-            cntDnis1 = cv2.findContours(closing1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            cntDnis2 = cv2.findContours(closing2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            # alternativa para el contorno mas grande
-            cntDnis1 = cntDnis1[0] if len(cntDnis1) == 2 else cntDnis1[1]
-            cntDnis1 = sorted(cntDnis1, key=cv2.contourArea, reverse=True)[0]
-            cntDnis2 = cntDnis2[0] if len(cntDnis2) == 2 else cntDnis2[1]
-            cntDnis2 = sorted(cntDnis2, key=cv2.contourArea, reverse=True)[0]
+            for c in contDnis1:
+                area = cv2.contourArea(c)
+                if minCnt < area < maxCnt:
+                    filtered.append(c)
 
-            cv2.drawContours(frame, [cntDnis1], -1, (0, 0, 255), 6) #sobre threshMan
-            cv2.drawContours(frame2, [cntDnis2], -1, (0, 255, 255), 6) #sobre threshAuto
+            if filtered != []:
+                #previene que rompa el programa cuando no encuentra ningún contorno que cumpla las specs
+                cnt = filtered[0]
+                max_area = cv2.contourArea(cnt)
+                #toma el contorno mas grande de los filtrados
+                for cont in filtered:
+                    if cv2.contourArea(cont) > max_area:
+                        cnt = cont
+                        max_area = cv2.contourArea(cont)
+                cv2.drawContours(frame, [cnt], -1, (0, 255, 255), 12, cv2.LINE_AA)
 
-
+            moments = cv2.moments(cnt)
+            huMoments = np.array(cv2.HuMoments(moments), dtype=float)
+            logTransf = -1*np.sign(huMoments) * np.log10(np.abs(huMoments))
 
         cv2.imshow(window_name, cv2.flip(frame, 1))
-        cv2.imshow(window_name2, cv2.flip(frame2, 1))
-
-
+        cv2.imshow(window_name2, cv2.flip(thresh1, 1))
 
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
+            print(filtered)
+            # print(cnt)
             break
+
+        if cv2.waitKey(1) & 0xFF == ord('c'):
+            #guardo el valor de lo que quiero comparar con huMoments
+            compare = cnt
+            break
+
 
 Tp()
