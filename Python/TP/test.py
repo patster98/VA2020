4acfc70890ee96923e9c390f81ca4e46f49da593
patster98 <57993483+patster98@users.noreply.github.com>
2020-09-17 15:05:50 -0300

    changes 17/9/2020 added to test.py: in line 105 added compare var to select contour to match

--- Python/TP/test.py
+++ Python/TP/test.py
@@ -30,6 +30,7 @@ cv2.createTrackbar(trackbar4_name, window_name, 800, 1500, on_trackbar_change)
 def Tp():
     cap = cv2.VideoCapture(0)
     comp = None
+
     while True:
         filtered = []
         similar = []
@@ -44,6 +45,7 @@ def Tp():
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         _, thresh1 = cv2.threshold(gray, threshVal_trackbar, 250, cv2.THRESH_BINARY)
+        # _, thresh2 = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, threshVal_trackbar, 0)
 
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
         opening1 = cv2.morphologyEx(thresh1, cv2.MORPH_OPEN, kernel)
@@ -79,34 +81,35 @@ def Tp():
                 logTransf = -1 * np.sign(huMoments) * np.log10(np.abs(huMoments))
                 cv2.drawMarker(frame, (cX1, cY1), (0, 0, 0), cv2.MARKER_CROSS, markerSize=20, thickness=1, line_type=8)
 
-            for filter in filtered:
-                err = cv2.matchShapes(filter, cnt, cv2.CONTOURS_MATCH_I1, 0)
-                if err < minError / 100 and cnt != []:
-                    similar.append(filter)
-                    mom1 = cv2.moments(filter)
-                    cX1 = int(mom1["m10"] / mom1["m00"])
-                    cY1= int(mom1["m01"] / mom1["m00"])
-                    cv2.putText(frame, "True", (cX1, cY1), cv2.FONT_ITALIC, 1, (0, 0, 0), 2, cv2.LINE_4)
-                else:
-                    noSimilar.append(filter)
-                    mom = cv2.moments(filter)
-                    cX = int(mom["m10"] / mom["m00"])
-                    cY = int(mom["m01"] / mom["m00"])
-                    cv2.putText(frame, "False", (cX, cY), cv2.FONT_ITALIC, 1, (0, 0, 0), 2, cv2.LINE_4)
+            if comp != []:
+                for filter in filtered:
+                    err = cv2.matchShapes(filter, comp, cv2.CONTOURS_MATCH_I1, 0)
+                    if err < minError / 100:
+                        similar.append(filter)
+                        mom1 = cv2.moments(filter)
+                        cX1 = int(mom1["m10"] / mom1["m00"])
+                        cY1 = int(mom1["m01"] / mom1["m00"])
+                        cv2.putText(frame, "True", (cX1, cY1), cv2.FONT_ITALIC, 1, (0, 0, 0), 2, cv2.LINE_4)
+                    else:
+                        noSimilar.append(filter)
+                        mom = cv2.moments(filter)
+                        cX = int(mom["m10"] / mom["m00"])
+                        cY = int(mom["m01"] / mom["m00"])
+                        cv2.putText(frame, "False", (cX, cY), cv2.FONT_ITALIC, 1, (0, 0, 0), 2, cv2.LINE_4)
 
-            cv2.drawContours(frame, noSimilar, -1, (0, 0, 255), 4, cv2.LINE_AA)
-            cv2.drawContours(frame, similar, -1, (0, 255, 0), 4, cv2.LINE_AA)
+                cv2.drawContours(frame, noSimilar, -1, (0, 0, 255), 4, cv2.LINE_AA)
+                cv2.drawContours(frame, similar, -1, (0, 255, 0), 4, cv2.LINE_AA)
 
         cv2.imshow(window_name, frame)
         cv2.imshow(window_name2, cv2.flip(thresh1, 1))
-
         if cv2.waitKey(1) & 0xFF == ord('c'):
-            # guardo el valor de lo que quiero comparar con huMoments
             comp = cnt
-
+            print(comp)
+            # guardo el valor de lo que quiero comparar con huMoments
+            continue
         if cv2.waitKey(1) & 0xFF == ord('q'):
             print(err)
-            print(comp)
+            # print(comp)
             # print("cnt Moments: ", Moments)
             # print("cnt HuMoments: ", huMoments)
             break
