3b34e35dfaaa8146429325f67d7a43b11a6725c0
patster98 <57993483+patster98@users.noreply.github.com>
2020-10-29 15:04:32 -0300

    changes 29/10/2020: added euclidean distance compare for bounding boxes Project still not working as intended (how to empty boundig boxes drawings?)

--- Python/TP2Tracking/testVideoTracking.py
+++ Python/TP2Tracking/testVideoTracking.py
@@ -2,7 +2,6 @@ import cv2
 import sys
 import numpy as np
 import imutils
-from pathlib import Path
 from utils.bgSubstract import substractbg
 
 ############### Tracker Types #####################
@@ -26,68 +25,53 @@ back_sub = cv2.createBackgroundSubtractorMOG2(history=700,
 
 videoPath = '/Users/Skyguy/Desktop/Ingenieria/SegundoCuatrimestre/VisionArtificial/VA2020/Python/static/videos' \
             '/carsRt9_3.avi'
-# videoPath = 0
-# videoPath = str(Path('static/videos/race.mp4').resolve(strict=False))
 
 cap = cv2.VideoCapture(videoPath)
-success, frame = cap.read()
+success,_ = cap.read()
+
 cnt = []
-box2 = []
-start = None
+box2 = ()
+x = 0
+y = 0
+x2 = 0
+y2 = 0
+start = False
 
 if not success:
     print('Video not found')
-    print(success)
+    print("success:", success, sep=" ")
     sys.exit(1)
-
-# bbox = cv2.selectROI("Tracking", frame, False)
-# tracker.init(frame, bbox)
-
-
-# def drawBox(img, bbox):
-#     x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
-#     cv2.rectangle(img, (x, y), ((x + w), (y + h)), (255, 0, 255), 3, 3)
-#     cv2.putText(img, "Tracking", (100, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
-
 
 while True:
 
     timer = cv2.getTickCount()
-    ret, img = cap.read()
+    ret, frame = cap.read()
+    drawframe = frame
     _, img2 = cap.read()
 
-    if img is None:
+    if frame is None:
         break
 
-    # img = imutils.resize(img, width=600) por alguna razon esto no anda
-    (success, bbox) = tracker.update(img)
-
-    # if success:
-    # drawBox(img, bbox)
+    frame = imutils.resize(frame, width=600)
+    (success, bbox) = tracker.update(frame)
+    # print(bbox)
 
     for box in bbox:
-    # print(bbox)
-    # if bbox != ():
         (x, y, w, h) = [int(v) for v in box]
         # (x, y, w, h) = [bbox[0,0], bbox[0,1], bbox[0,2], bbox[0,3]]
-
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.putText(img, "Tracking", (100, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
-            # Draw circle in the center of the bounding box
+        rect = cv2.rectangle(drawframe, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(drawframe, "Tracking", (100, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+        # Draw circle in the center of the bounding box
         x2 = x + int(w / 2)
         y2 = y + int(h / 2)
-        cv2.circle(img, (x2, y2), 4, (0, 255, 0), -1)
+        cv2.circle(drawframe, (x2, y2), 4, (0, 255, 0), -1)
     # else:
     #     cv2.putText(img, "Lost", (100, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
-    cv2.rectangle(img, (15, 15), (200, 90), (255, 0, 255), 2)
-    cv2.putText(img, "Fps:", (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 255), 2);
-    cv2.putText(img, "Status:", (20, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 255), 2);
+    cv2.rectangle(drawframe, (15, 15), (200, 90), (255, 0, 255), 2)
+    cv2.putText(drawframe, "Fps:", (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 255), 2);
+    cv2.putText(drawframe, "Status:", (20, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 255), 2);
 
-    # if success is False:
-    #     # bbox = []
-    #     bbox = cv2.selectROI("Tracking", img, False)
-    #     print()
 
     fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
     if fps > 60:
@@ -96,46 +80,41 @@ while True:
         myColor = (230, 20, 20)
     else:
         myColor = (20, 20, 230)
-    cv2.putText(img, str(int(fps)), (75, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, myColor, 2);
-
-    cv2.imshow("Tracking", img)
-    key = cv2.waitKey(1) & 0xFF
+    cv2.putText(drawframe, str(int(fps)), (75, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, myColor, 2);
 
     # cnt.append(cnt)
     cnt, bwimg = substractbg(img2, back_sub)
     if cnt != [] and start:
-        # print(cnt)
-        if box2 != []:
-            box2 = []
 
         x, y, w, h = cv2.boundingRect(cnt)
         # print(x,y,w,h)
-        # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0),
+        box2 = (x,y,w,h)
 
-        box2.append(x)
-        box2.append(y)
-        box2.append(w)
-        box2.append(h)
+        # comparar con el ultimo box y si no esta cerca no lo agrego
+        compare = ((x - x2)**2 + (y-y2)**2)**0.5
+        # print("compare:", compare, sep=" ")
 
-
-
-        # Print the centroid coordinates (we'll use the center of the
-        # # bounding box) on the image
-        # text = "x: " + str(x2) + ", y: " + str(y2)
-        # cv2.putText(frame, text, (x2 - 10, y2 - 10),
-        #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-        tracker.add(argtrack, img, tuple(box2))
+        if float(compare) < 50:
+            tracker.add(argtrack, frame, box2)
+        else:
+            frame = img2.copy()
+            frame = imutils.resize(frame, width=600)
+            bbox = ()
+        cnt = []
         cv2.imshow("Black and white", bwimg)
+    x2 = x
+    y2 = y
+    cv2.imshow("Tracking", drawframe)
+    key = cv2.waitKey(1) & 0xFF
 
     if key == ord('b'):
         start = True
 
     if key == ord('s'):
-        box = cv2.selectROI("Tracking", img, fromCenter=False,
+        box = cv2.selectROI("Tracking", frame, fromCenter=False,
                             showCrosshair=True)
         # tracker1 = argtrack
-        tracker.add(argtrack, img, box)
+        tracker.add(argtrack, frame, box)
         # print(box)
 
     if key == ord('q'):
